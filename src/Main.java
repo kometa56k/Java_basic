908f96346393e8cded89ec8d65ac2ed3e5ecdaad
MarinaKolosova <MarinaKolosova@x5.ru>
2023-02-05 15:22:37 +0700

    Task 2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+// 1. Создайте переменную типа с плавающей точкой с наибольшим диапазоном чисел. Присвойте ей произвольное дробное значение
+//
+//2. Создайте переменную целочисленного типа с наименьшим диапазоном чисел. Присвойте ей наибольшее возможное значение
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void task1(String[] args) {
+        System.out.println("Hello World!");
+    }
+    public static void task2(String[] args) {
+        double num1 = 1789054.7564;
+        byte num2 = 127;
+        System.out.println("Переменная типа doouble" + num1);
+        System.out.println("Переменная типа byte" + num2);
     }
 }
