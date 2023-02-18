71f656a4d9417abddd6cb206b395de33628602ff
MarinaKolosova <MarinaKolosova@x5.ru>
2023-02-18 17:15:04 +0700

    Решение Task 4

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
+/*
+Написать программу, которая будет выполнять следующие действия:
+
+1. Ввести три числа с клавиатуры x, y, z
+
+2. Найти и вывести в консоль среднее арифметическое этих чисел
+
+3. Разделить среднее арифметическое на 2 и округлить в меньшую сторону
+
+4. Если полученное число больше 3, то вывести на экран сообщение "Программа выполнена корректно"
+ */
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите число х:");
+        float x = in.nextFloat();
+        System.out.println("Введите число y:");
+        float y = in.nextFloat();
+        System.out.println("Введите число z:");
+        float z = in.nextFloat();
+        float SrNum = (x+y+z)/3;
+        System.out.println("Среднее арифметическое равно: " + SrNum);
+        float SrNum2 = SrNum/=2;
+        if (Math.floor(SrNum2) > 3.0) {
+            System.out.println("Программа выполнена корректно");
+        }
+        else
+            System.out.println("Результат равен или меньше 3: " + Math.floor(SrNum2));
+        }
     }
-}
