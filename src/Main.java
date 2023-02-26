499c211e5931a7c515e7d1e5badf2aae56684680
MarinaKolosova <MarinaKolosova@x5.ru>
2023-02-26 15:55:39 +0700

    task 13

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
+/*
+Напишите программу, где пользователь должен ввести строку из слов, разделенных пробелами.
+Например: “I love java 8 Я люблю java 14 core1”. Выполните следующие действия:
+1. Выведите слова, состоящие только из латиницы.
+2. Выведите количество этих слов.
+ */
+import java.util.Scanner;
 public class Main {
+    static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Введите строку, например: I love java 8 Я люблю java 14 core1");
+        String result = in.nextLine();
     }
 }
