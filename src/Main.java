e441437f68a3ae39c86b08cbb9b5a242b8c26f9f
MarinaKolosova <MarinaKolosova@x5.ru>
2023-02-26 17:05:17 +0700

    Решение task 13

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,17 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Введите строку, например: I love java 8 Я люблю java 14 core1");
         String result = in.nextLine();
+        System.out.println(result);
+        String[] masresult = result.split(" ");
+        int lengthmas = masresult.length;
+        System.out.println("Общее количество слов: " + lengthmas);
+        String result2 = result.replaceAll("[А-Яа-я0-9]","");
+        result2 = result2.replaceAll("  "," ");
+        result2 = result2.replaceAll("  "," ");
+        System.out.println(result2);
+        result2 = result2.trim();
+        String[] masresult2 = result2.split(" ");
+        int lengthmas2 = masresult2.length;
+        System.out.println("Общее количество слов из латиницы: " + lengthmas2);
     }
 }
