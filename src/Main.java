f83f30a584c43ffe6514e5dcc1ac32d54bdb76e9
MarinaKolosova <MarinaKolosova@x5.ru>
2023-02-26 15:50:31 +0700

    Решение task 12

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+/*
+Напишите программу, принимающую на вход строку “I like Java!!!”, которая будет выполнять следующие действия:
+1. Проверить, содержит ли строка подстроку “Java” (используйте contains()).
+2. Проверить, начинается ли строка с подстроки “I like” (используйте startsWith()).
+3. Проверить, заканчивается ли строка с подставки “!!!” (используйте endsWith()).
+4. Если 3 предыдущих условия верны, выведите данную строку, преобразованную к верхнему регистру.
+5. Замените все символы ‘a’ на ‘о’ и введите подстроку “Jovo” на экран (используйте substring()).
+ */
+import java.util.Scanner;
 public class Main {
+    static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Введите строку: I like Java!!!");
+        String result = in.nextLine();
+        if (result.contains("Java")) {
+            if (result.startsWith("I like")) {
+                if (result.endsWith("!!!")) {
+                    String result1 = result.toUpperCase();
+                    result = result.replaceAll("a","o");
+                    System.out.println(result1);
+                    System.out.println(result.substring(7,11));
+                }
+            }
+        }
     }
 }
