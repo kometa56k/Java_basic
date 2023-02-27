5751b12c33a17743e6ec94a7eedf712f89ceef1a
MarinaKolosova <MarinaKolosova@x5.ru>
2023-02-27 10:13:45 +0700

    Решение final task 1

--- src/Main.java
+++ src/Main.java
@@ -8,8 +8,16 @@
 - Количество рублей: 1000
 - Итого: 14,80 долларов
  */
+import java.util.IllegalFormatPrecisionException;
+import java.util.Scanner;
 public class Main {
+    static Scanner in = new Scanner(System.in);
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Введите текущий курс доллара:");
+        Double course = in.nextDouble();
+        System.out.println("Введите количество рублей:");
+        int rub = in.nextInt();
+        Double result = rub / course;
+        System.out.printf("Итого: %.2f долларов",result);
     }
 }
